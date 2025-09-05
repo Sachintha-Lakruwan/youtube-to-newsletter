f35e530c292637b286ea65b4cbb418800da4d17d
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2025-09-05 22:26:45 +0530

    add firebase backend config

--- frontend/react-app/src/firebase.js
+++ frontend/react-app/src/firebase.js
@@ -1,16 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
-};
+let app;
+let auth;
 
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
+export async function initFirebase() {
+  if (app) return { app, auth };
+
+  const res = await fetch("/config");
+  const firebaseConfig = await res.json();
+
+  app = initializeApp(firebaseConfig);
+  auth = getAuth(app);
+
+  return { app, auth };
+}
