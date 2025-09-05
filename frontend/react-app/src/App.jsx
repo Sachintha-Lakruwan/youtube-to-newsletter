4bba50e82fe50084a2a0b6126473d56aa392898b
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2025-09-05 21:10:18 +0530

    Add dummy newsletters page

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Preferences from "./Preferences";
+import NewsLetters from "./NewsLetters";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/preferences" element={<Preferences />} />
+        <Route path="/newsletters" element={<NewsLetters />} />
       </Routes>
     </Router>
   );
---
+++ frontend/react-app/src/NewsLetters.jsx
@@ -0,0 +1,7 @@
+import Wrapper from "./Background";
+
+function NewsLetters() {
+  return <Wrapper>NewsLetters are under construction 🚧</Wrapper>;
+}
+
+export default NewsLetters;
